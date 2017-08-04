901a67f75a0252cca3bb7b772799bdee243f94e5
Sean Nelson <sanelson87@gmail.com>
2017-08-04 20:12:37 +0100

    Updated dependencies #2 material peer reqs met

--- src/app/google-books.service.ts
+++ src/app/google-books.service.ts
@@ -1,4 +1,5 @@
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
